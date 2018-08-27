75746615100f0581aa9b11acc7ead8e13b5c6e21
Gunnar Andreas Skogvold <andreas.skogvold@conceptos.no>
2018-08-27 10:56:02 +0200

    Added support for setting port number through options

--- src/main.c
+++ src/main.c
@@ -3,17 +3,67 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
-int main(void)
+#define DEFAULT_PORT 5045
+
+typedef struct
 {
-    const unsigned int server_port = 0x5545;
-    DiagnosticsServerPtr server = createServer(server_port);
+    unsigned int port;
+} InputOptions;
+
+void printUsage()
+{
+    printf("Usage: tcp_reactor -p port\n");
+}
+//Returns -1 on failure
+int parseInputOptions(int argc, char *argv[], InputOptions *options)
+{
+    if (options == NULL)
+    {
+        return -1;
+    }
+
+    const char* valid_option = "p:";
+    int num_of_options_parsed = 0;
+    int c;
+
+    while((c = getopt(argc, argv, valid_option)) != -1)
+    {
+        switch (c)
+        {
+            case 'p':
+                options->port = atoi(optarg);
+                break;
+            default:
+                printUsage();
+                exit(EXIT_FAILURE);
+        }
+        num_of_options_parsed++;
+    }
+    return num_of_options_parsed;
+}
+
+int main(int argc, char *argv[])
+{
+    InputOptions options = {DEFAULT_PORT};
+    if (parseInputOptions(argc, argv, &options) == -1)
+    {
+        printf("Input parsing failure\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Creating server with port nr %d\n", options.port);
+
+    DiagnosticsServerPtr server = createServer(options.port);
 
     if (NULL == server)
     {
         printf("Failed to create the server");
         exit(EXIT_FAILURE);
     }
+    
+
 
     /* Enter the eternal reactive event loop. */
     while(1)
